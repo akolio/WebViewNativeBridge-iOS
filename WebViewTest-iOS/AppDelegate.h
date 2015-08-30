3e758a099b86f11830aeb66f7185068387ceeea3
Antti Köliö <antti.kolio@gofore.com>
2015-08-30 15:01:15 +0300

    Initial Commit

---
+++ WebViewTest-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebViewTest-iOS
+//
+//  Created by Antti Köliö on 30/08/15.
+//  Copyright (c) 2015 Antti Köliö. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebViewTest-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebViewTest-iOS
+//
+//  Created by Antti Köliö on 30/08/15.
+//  Copyright (c) 2015 Antti Köliö. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
